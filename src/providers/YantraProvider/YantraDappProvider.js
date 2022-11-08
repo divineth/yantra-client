faf42685b2ed11309beddfe7820044077073e7aa
divineth <divinethomson@outlook.com>
2022-11-08 07:08:07 +0000

    Experimental switch of providers

--- src/providers/YantraProvider/YantraDappProvider.js
+++ src/providers/YantraProvider/YantraDappProvider.js
@@ -1,10 +1,43 @@
 import React, { useEffect, useState } from "react";
-import { Localhost, Mumbai, useEthers } from "@usedapp/core";
+import {
+  ChainId,
+  Mumbai,
+  useEthers,
+  useUpdateConfig,
+} from "@usedapp/core";
 import YantraDappContext from "./context";
 
 function YantraDappProvider({ children }) {
-  const { account, chainId } = useEthers();
+  const { account, chainId, library } = useEthers();
+  const updateConfig = useUpdateConfig();
   const [isChainError, setIsChainError] = useState(false);
+
+
+  // switch to the connectors provider whenever the account is changed
+  // help to reduce the number of calls to the alchemy node
+  // needs further testing
+  useEffect(() => {
+    try {
+      if (account != undefined && library != undefined) {
+        updateConfig({ readOnlyUrls: { [ChainId.Mumbai]: library } });
+      } else {
+        updateConfig({
+          readOnlyUrls: {
+            [ChainId.Mumbai]:
+              "https://polygon-mumbai.g.alchemy.com/v2/fgk6Oa3fc9bndH31q-ahB6HwPRK25-mP",
+          },
+        });
+      }
+    } catch (e) {
+      console.error("Provider switch failed. Going back to alchemy: ", e);
+      updateConfig({
+        readOnlyUrls: {
+          [ChainId.Mumbai]:
+            "https://polygon-mumbai.g.alchemy.com/v2/fgk6Oa3fc9bndH31q-ahB6HwPRK25-mP",
+        },
+      });
+    }
+  }, [account]);
 
   useEffect(() => {
     if (account != undefined && chainId != undefined) {
