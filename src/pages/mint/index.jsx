395f2b9d6ae01302d14a62455bfdda3ce9acaee5
divineth <divinethomson@outlook.com>
2022-11-17 23:29:58 +0000

    Modify styling for mint page

--- src/pages/mint/index.jsx
+++ src/pages/mint/index.jsx
@@ -8,7 +8,7 @@ export default function Mint() {
       <Head>
         <title>$YANTRA | NFT</title>
       </Head>
-      <div className="flex flex-col px-4 md:px-32 lg:px-8 xl:px-0 pt-12 pb-4 lg:pb-4 gap-8 lg:flex-row justify-center items-center">
+      <div className="flex flex-col px-4 md:px-32 lg:px-8 xl:px-0 pt-4 md:pt-8 pb-4 lg:pb-4 gap-8 lg:flex-row justify-center items-center">
         <NFTBox />
         <MintWidget />
       </div>
